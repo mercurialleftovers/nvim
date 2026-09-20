fa1480914adcbd7a265265e35d9f4479259337d7
mercurialleftovers <mercurialleftovers@proton.me>
2026-09-21 00:04:09 +0100

    first

---
+++ init.lua
@@ -0,0 +1,129 @@
+-- PLUGINS
+require("config.lazy")
+-- globals
+NMODE_TERM = "<C-\\><C-n>"
+FONTNAME = "Consolas:h13"
+FONTNAME = "Lucida Console:h12"
+COLORTHEME = "slate"
+INTERPRETERS = {}
+INTERPRETERS["python"] = "python -i fname"
+TEMPDIR = vim.fs.joinpath(vim.fn.stdpath("data"), "tempstuff")
+UNDODIR = vim.fs.joinpath(TEMPDIR, "undodir")
+BUILD_SCRIPT = "build.bat" -- TODO(bader): add Nix support (build.sh)
+if vim.fn.isdirectory(UNDODIR) == 0 then vim.fn.mkdir(UNDODIR, "p") else print(UNDODIR .. " is a dir") end
+-- settings
+vim.cmd("colorscheme " .. COLORTHEME)
+vim.o.guifont = FONTNAME
+vim.o.showmode = false
+vim.o.wrap = false
+vim.opt.scrolloff = 20
+vim.opt.signcolumn = "yes:2"
+-- completion
+vim.opt.complete = "o,.,b"
+vim.opt.completeopt = "menu,menuone,fuzzy,preview,popup,noselect,noinsert"
+vim.opt.autocomplete = true
+-- splitting
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+-- clipboard
+vim.opt.clipboard = "unnamedplus"
+-- undodir
+vim.opt.undodir = UNDODIR
+vim.opt.swapfile = false
+-- tabs
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+-- keymaps:
+-- ctrl-s
+vim.keymap.set({ "n", "v", "i" }, "<C-s>", "<Esc>:w<Enter>", { silent = true, noremap = true })
+-- ctrl-c
+vim.keymap.set({ "v" }, "<C-c>", "y", { silent = true, noremap = true })
+vim.keymap.set({ "n", "i" }, "<C-c>", "<Esc>yy", { silent = true, noremap = true })
+-- ctrl v
+vim.keymap.set({ "v" }, "<C-v>", "p", { silent = true, noremap = true })
+vim.keymap.set({ "n", "i" }, "<C-v>", "<Esc>p", { silent = true, noremap = true })
+-- ctrl x
+vim.keymap.set({ "n", "i" }, "<C-x>", "<Esc>dd", { silent = true, noremap = true })
+vim.keymap.set({ "v" }, "<C-x>", "x", { silent = true, noremap = true })
+-- emacs navigation
+vim.keymap.set({ "i" }, "<C-a>", "<Esc>^i", { silent = true, noremap = true })
+vim.keymap.set({ "i" }, "<C-e>", "<Esc>$a", { silent = true, noremap = true })
+vim.keymap.set({ "v", "n" }, "<C-a>", "^", { silent = true, noremap = true })
+vim.keymap.set({ "v", "n" }, "<C-e>", "$", { silent = true, noremap = true })
+-- terminals
+vim.keymap.set({ "i", "v", "n", "t" }, "<C-Enter>", function() vim.cmd(":hor term") end,
+	{ silent = true, noremap = true })
+vim.keymap.set({ "t" }, "<Esc>", NMODE_TERM, { silent = true, noremap = true })
+-- ctrl-q
+vim.keymap.set({ "i", "v", "n", }, "<C-q>", "<Esc>:q!<Enter>", { silent = true, noremap = true })
+vim.keymap.set({ "t", }, "<C-q>", NMODE_TERM .. ":q!<Enter>", { silent = true, noremap = true })
+-- tab and esc completion stuff
+vim.keymap.set({ "i", }, "<Tab>", function() if vim.fn.pumvisible() == 1 then return "<C-n>" else return "<Tab>" end end,
+	{ silent = true, noremap = true, expr = true })
+vim.keymap.set({ "i", }, "<S-Tab>",
+	function() if vim.fn.pumvisible() == 1 then return "<C-p>" else return "<S-Tab>" end end,
+	{ silent = true, noremap = true, expr = true })
+vim.keymap.set({ "i", }, "<Esc>", function() if vim.fn.pumvisible() == 1 then return "<C-e>" else return "<Esc>" end end,
+	{ silent = true, noremap = true, expr = true })
+-- tabs
+vim.keymap.set({ "n", "v", "i", "t" }, "<C-t>", function() vim.cmd(":tabnew") end, { silent = true, noremap = true })
+vim.keymap.set({ "n", "v", "i", "t" }, "<a-h>", function() vim.cmd(":tabprevious") end, { silent = true, noremap = true })
+vim.keymap.set({ "n", "v", "i", "t" }, "<a-l>", function() vim.cmd(":tabnext") end, { silent = true, noremap = true })
+vim.keymap.set({ "n", "v", "i", "t" }, "<S-a-h>", function() vim.cmd(":tabmove -1") end,
+	{ silent = true, noremap = true })
+vim.keymap.set({ "n", "v", "i", "t" }, "<S-a-l>", function() vim.cmd(":tabmove +1") end,
+	{ silent = true, noremap = true })
+-- windows
+for _, val in pairs({ "h", "j", "k", "l" }) do
+	vim.keymap.set({ "n", "v", "i" }, "<C-" .. val .. ">", "<Esc><C-w>" .. val, { silent = true, noremap = true })
+	vim.keymap.set({ "t" }, "<C-" .. val .. ">", NMODE_TERM .. "<C-w>" .. val, { silent = true, noremap = true })
+end
+-- indentation
+vim.keymap.set({ "v" }, "<Tab>", ">gv", { silent = true, noremap = true })
+vim.keymap.set({ "v" }, "<S-Tab>", "<gv", { silent = true, noremap = true })
+-- Lex
+vim.keymap.set({ "n", "v", "i", "t" }, "<C-b>", function() vim.cmd(":Lex 20") end, { silent = true, noremap = true })
+-- <F5>: runfile
+function RunFile(filepath)
+	if filepath == nil then filepath = vim.fn.substitute(vim.fn.expand("%:p"), "\\", "/", "g") end
+	local cmd = INTERPRETERS[vim.o.filetype]
+	if not cmd then return end
+	cmd = vim.fn.substitute(cmd, "fname", filepath, "g")
+	vim.cmd(":tabnew | term " .. cmd)
+end
+
+vim.keymap.set({ "n", "v", "i" }, "<F5>", RunFile, { silent = true, noremap = true })
+-- <S-F5>: runfile
+function RunBuildScript()
+	local build_file = vim.fn.findfile(BUILD_SCRIPT, ".;", -3)
+	if #build_file == 0 then return end
+	build_file = build_file[1]
+	local parent_dir = vim.fn.fnamemodify(build_file, ":h:p")
+	vim.cmd("tcd " .. parent_dir .. " | tabnew | term " .. build_file)
+end
+
+vim.keymap.set({ "n", "v", "i" }, "<S-F5>", RunBuildScript, { silent = true, noremap = true })
+-- <S-Enter>: RunChunk
+function RunChunk(filepath)
+	local filepath = vim.fs.joinpath(TEMPDIR, "temp_file." .. vim.o.filetype)
+	vim.cmd("normal! \"cy")
+	vim.fn.writefile(vim.fn.getreg('c'), filepath)
+	RunFile(filepath)
+end
+
+vim.keymap.set({ "v" }, "<S-Enter>", RunChunk, { silent = true, noremap = true })
+-- font increase/decrese
+function ChangeFontSize(size)
+	local nbr_pattern = '\\d\\+'
+	local current_size = tonumber(vim.fn.matchstr(vim.o.guifont, nbr_pattern))
+	if current_size == nil then return end
+	vim.o.guifont = vim.fn.substitute(vim.o.guifont, nbr_pattern, current_size + size, "")
+end
+
+vim.keymap.set({ "i", "n", "v" }, "<C-=>", function() ChangeFontSize(1) end, { silent = true, noremap = true })
+vim.keymap.set({ "i", "n", "v" }, "<C-->", function() ChangeFontSize(-1) end, { silent = true, noremap = true })
+-- move selection up and down + gq if formatting capability supplied by lsp
+vim.keymap.set("v", "<S-k>", ":m '<-2<CR>gv=gv", { silent = true, noremap = silent })
+vim.keymap.set("v", "<S-j>", ":m '>+1<CR>gv=gv", { silent = true, noremap = silent })
+-- cd into the config dir
+function godir() vim.cmd("tcd " .. vim.fn.stdpath("config")) end
---
+++ lua/plugins/plugins.lua
@@ -0,0 +1,100 @@
+return {
+	{
+		'nvim-mini/mini.icons',
+		version = '*',
+		config = function()
+			require("mini.icons").setup()
+		end,
+	},
+	{
+		'nvim-mini/mini.pairs',
+		version = '*',
+		config = function()
+			require("mini.pairs").setup()
+		end,
+	},
+	{
+		'nvim-mini/mini.surround',
+		version = '*',
+		config = function()
+			require("mini.surround").setup()
+		end,
+	},
+	{
+		'nvim-mini/mini.files',
+		version = '*',
+		config = function()
+			require("mini.files").setup()
+		end,
+	},
+	-- { 'nvim-mini/mini.pairs', version = '*', config = function() require("mini.pairs").setup({modes = {insert = true, command = true, terminal = true}), },
+	{ "https://github.com/m4xshen/autoclose.nvim", config = function() require("autoclose").setup() end },
+	{
+		'nvim-telescope/telescope.nvim',
+		version = '*',
+		dependencies = {
+			'nvim-lua/plenary.nvim',
+			-- optional but recommended
+			{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+		},
+		config = function()
+			local builtin = require('telescope.builtin')
+			vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+			vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+			vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+			vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+		end,
+	},
+	-- { "mason-org/mason.nvim", opts = {}, }, -- handled by dependencies
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {},
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
+		},
+		config = function()
+			require("mason-lspconfig").setup {
+				ensure_installed = { "lua_ls", "pyright", "ruff" },
+			}
+
+			-- configuring lsp stuff
+			local lsp_grp = vim.api.nvim_create_augroup("LspGroup", { clear = true })
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = lsp_grp,
+				callback = function(ev)
+					print("LspAttached :")
+					print(vim.inspect(vim.lsp.get_clients({ bufnr = 0 })))
+
+					local opts = { buffer = ev.buf }
+
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+					vim.keymap.set("n", "<leader>i", vim.lsp.buf.implementation, opts)
+					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+					vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+					vim.keymap.set("n", "<leader>D", vim.diagnostic.open_float, opts)
+					vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+					vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+				end
+			})
+
+			-- autoformatting with lsp
+			vim.api.nvim_create_autocmd(
+				{
+					"BufWritePre",
+				},
+				{
+					pattern = { '*' },
+					callback = function(args)
+						if #vim.lsp.get_clients({ bufnr = 0 }) == 0 then return end
+						vim.lsp.buf.format({ bufnr = args.buf })
+					end,
+					group = lsp_grp,
+				}
+			)
+		end
+	}
+}
